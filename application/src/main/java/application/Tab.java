80f0770924633321745b61d303728e169314291c
Charlotte Lam <ZLAC188@live.rhul.ac.uk>
2024-03-12 17:00:55 +0000

    BE: New Tabs object created with fields of private G,D,A and E lines

---
+++ application/src/main/java/application/Tab.java
@@ -0,0 +1,25 @@
+package application;
+
+public class Tab {
+    private String G = "G|";
+    private String D = "D|"; 
+    private String A = "A|";
+    private String E = "E|";
+
+    public String getG(){
+        return G;
+    }
+
+    public String getD(){
+        return D;
+    }
+
+    public String getA(){
+        return A;
+    }
+
+    public String getE(){
+        return E;
+    }
+
+}
